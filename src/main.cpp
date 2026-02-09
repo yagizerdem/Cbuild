ee244ed83da784ebe0493b14bb5d2a59722e679c
yagiz erdem <2yagiz004@gmail.com>
2026-02-09 18:36:06 +0300

    backward cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#if defined(_WIN32)
+#if defined(_MSC_VER)
+	#define USE_BACKWARD 1
+#else
+	#define USE_BACKWARD 0
+#endif
+#else
+#define USE_BACKWARD 1
+#endif
+
+#if USE_BACKWARD
+#include "backward/backward.hpp"
+static backward::SignalHandling g_signal_handler;
+#endif
+
 
 #include "stringUtil.h"
 
 int main() {
-
 
 	return 0;
 }
