38f864c09fdc2c7a6b37948e0f92f8e8846ab396
yagiz erdem <yagizerdem819@gmail.com>
2026-08-02 00:17:04 +0300

    refactor: move VM opcode and chunk classes to cBuildScript package

--- src/main/java/io/Cbuild/Main.java
+++ src/main/java/io/Cbuild/Main.java
@@ -8,7 +8,10 @@ import org.luaj.vm2.Globals;
 
 public class Main {
     public static void main(String[] args) {
+        cBuildMain();
+    }
 
+    public static void cBuildMain() {
         try {
             Globals globals = LuaGlobals.getGlobalsCached();
             io.Cbuild.lua.luaNatives.globals.register(globals);
