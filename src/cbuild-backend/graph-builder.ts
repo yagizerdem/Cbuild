de2a01b5bf38722f04ca57e1bb520e3ff9b6b712
yagiz erdem <2yagiz004@gmail.com>
2026-09-07 21:58:54 +0300

    feat: core module

--- src/cbuild-backend/graph-builder.ts
+++ src/cbuild-backend/graph-builder.ts
@@ -306,34 +306,3 @@ function topologicalSortRecursive(
   visitedTargets.add(target);
   sorted.push(...targetRules);
 }
-
-export async function shouldRebuild(
-  rule: NormalRule,
-  baseDir: string,
-): Promise<boolean> {
-  const targetEntryPath: string = resolveAndGetAbsolutePath(
-    baseDir,
-    rule.target,
-  );
-
-  if (!(await fileExistbyAbsolutePathAsync(targetEntryPath))) return true;
-
-  const lastModifiedDateOfTarget: bigint | undefined =
-    await getModifiedTimeNsAsync(targetEntryPath);
-
-  if (!lastModifiedDateOfTarget) return true;
-
-  for (const preq of rule.prerequisites) {
-    const preqAbsolutePath = resolveAndGetAbsolutePath(baseDir, preq);
-    const exist: boolean = await fileExistbyAbsolutePathAsync(preqAbsolutePath);
-    if (!exist) return true;
-
-    const lastModifiedDateOfPreq: bigint | undefined =
-      await getModifiedTimeNsAsync(preqAbsolutePath);
-
-    if (!lastModifiedDateOfPreq) return true;
-    if (lastModifiedDateOfPreq > lastModifiedDateOfTarget) return true;
-  }
-
-  return false;
-}
