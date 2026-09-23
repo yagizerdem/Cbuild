43bdf98a6b074b40ed1226b8bbb4310eedcac4d8
yagiz erdem <2yagiz004@gmail.com>
2026-09-23 14:57:15 +0300

    refactor: build-file evaluation state object created

--- src/cbuild-backend/evaluator/vpath-evaluator.ts
+++ src/cbuild-backend/evaluator/vpath-evaluator.ts
@@ -1,39 +1,45 @@
 import { Env } from "@cbuild-backend/env.js";
 import { ValueExpansionEngine } from "@cbuild-backend/expansion.js";
 import { VpathIR } from "@src/compiler/ir.js";
-import { VpathRule } from "../model.js";
+import { VpathRule } from "@cbuild-backend/model.js";
+import { BuildFileEvaluationState } from "@cbuild-backend/evaluator/core/type.js";
 
 export default class VpathIREvaluator {
   private readonly context: Env;
   private readonly valueExpansionEngine: ValueExpansionEngine;
   private readonly ir: VpathIR;
-  constructor(context: Env, ir: VpathIR) {
+  private readonly evaluationState: BuildFileEvaluationState;
+  constructor(
+    context: Env,
+    ir: VpathIR,
+    evaluationState: BuildFileEvaluationState,
+  ) {
     this.context = context;
     this.ir = ir;
+    this.evaluationState = evaluationState;
     this.valueExpansionEngine = new ValueExpansionEngine(context);
   }
 
-  public execute(vpaths: VpathRule[]): VpathRule[] {
+  public execute() {
     if (this.ir.vpath.type === "clear-all") {
-      return [];
+      this.evaluationState.vpaths = [];
     } else if (this.ir.vpath.type === "clear-pattern") {
       const vpathPattern = this.valueExpansionEngine.exec(
         this.ir.vpath.pattern,
       );
 
-      vpaths = vpaths.filter((vpath) => vpath.pattern !== vpathPattern);
-      return vpaths;
+      this.evaluationState.vpaths = this.evaluationState.vpaths.filter(
+        (vpath) => vpath.pattern !== vpathPattern,
+      );
+    } else if (this.ir.vpath.type === "set-pattern") {
+      const vpathPattern = this.valueExpansionEngine.exec<string>(
+        this.ir.vpath.pattern,
+      );
+      const searchDirs: string[] = this.ir.vpath.directories.map((path) =>
+        this.valueExpansionEngine.exec(path),
+      );
+
+      this.evaluationState.vpaths.push(new VpathRule(vpathPattern, searchDirs));
     }
-
-    const vpathPattern = this.valueExpansionEngine.exec<string>(
-      this.ir.vpath.pattern,
-    );
-    const searchDirs: string[] = this.ir.vpath.directories.map((path) =>
-      this.valueExpansionEngine.exec(path),
-    );
-
-    vpaths.push(new VpathRule(vpathPattern, searchDirs));
-
-    return vpaths;
   }
 }
